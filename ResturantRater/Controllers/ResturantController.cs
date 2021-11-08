6d0dd3374de6fcbb5064120a53120885220d40ae
CJ <thompsoncj93@gmail.com>
2021-11-08 14:45:30 -0500

    addedEdit

--- ResturantRater/Controllers/ResturantController.cs
+++ ResturantRater/Controllers/ResturantController.cs
@@ -1,7 +1,9 @@
 ﻿using ResturantRater.Models;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 
@@ -61,6 +63,35 @@ namespace ResturantRater.Controllers
             _db.Resturants.Remove(resturant);
             _db.SaveChanges();
             return RedirectToAction("Index");
+        }
+
+        //Get: Resturant/Edit/{id}
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Resturant resturant = _db.Resturants.Find(id);
+            if (resturant == null)
+            {
+                return HttpNotFound();
+            }
+            return View(resturant);
+        }
+
+        //POST: Resturant/Edit/{id}
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Resturant resturant)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Entry(resturant).State = EntityState.Modified;
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(resturant);
         }
     }
 }
